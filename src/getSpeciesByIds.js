6862f537f1186071ee59943fd14a7097cb7be1ca
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-10-09 21:58:43 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+function getSpeciesByIds(...ids) {
+  const arrIds = [...ids];
+  return arrIds.map((arr) => species.map((specie) => specie)
+    .filter((specie) => specie.id.includes(arr))
+    .map((specie2) => specie2.name)).flat();
 }
 
 module.exports = getSpeciesByIds;
