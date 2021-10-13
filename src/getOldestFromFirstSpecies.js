0bf151c765a566722efb738338cf6120f39dc9a5
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-10-13 12:53:19 -0300

    Requisito - 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { employees, species } = data;
+  const [primeiroAnimal] = employees.filter((employer) => employer.id === id).flat()
+    .map((objeto) => objeto.responsibleFor[0]);
+  const objetoRetornado = species.filter((specie) => specie.id === primeiroAnimal)
+    .map((animal) => animal.residents).flat()
+    .reduce((acc, curr) => (acc.age > curr.age ? acc : curr))
+  return Object.values(objetoRetornado);
 }
 
 module.exports = getOldestFromFirstSpecies;
