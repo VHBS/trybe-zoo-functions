476e77e7b68cb94b3e8417789eec624b2eec29f2
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-10-14 01:13:37 -0300

    TODOS OS REQUISITOS

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
-const notIncludedName = () => { // !options || sem opção incluedNames.
+
+const valorBase = () => { // !options || sem opção incluedNames.
   const objetoRetornado = { };
   objetoRetornado.NE = species.filter((specie) => specie.location === 'NE')
     .map((mapNe) => mapNe.name);
@@ -14,77 +15,90 @@ const notIncludedName = () => { // !options || sem opção incluedNames.
   return objetoRetornado;
 };
 
-// const includeNames = () => { // Se o parametro possui incluedName.
-//   const regioes = Object.keys(notIncludedName());
-//   const objetoRetornado = { };
-//   regioes.forEach((regiao) => {
-//     const regis = species.filter((specie) => specie.location === regiao)
-//       .reduce((acc, curr) => {
-//         acc.push(({ [curr.name]: curr.residents.map((nomeAn) => nomeAn.name) }));
-//         return acc;
-//       }, []);
-//     objetoRetornado[regiao] = regis;
-//   });
-//   return objetoRetornado;
-// };
+const includeNamesTrue = () => { // Se o parametro possui incluedName.
+  const regioes = Object.keys(valorBase());
+  const objetoRetornado = { };
+  regioes.forEach((regiao) => {
+    const regis = species.filter((specie) => specie.location === regiao)
+      .reduce((acc, curr) => {
+        acc.push(({ [curr.name]: curr.residents.map((nomeAn) => nomeAn.name) }));
+        return acc;
+      }, []);
+    objetoRetornado[regiao] = regis;
+  });
+  return objetoRetornado;
+};
 
-// const includeSorted = () => {
-//   const regioes = Object.keys(notIncludedName());
-//   const objetoRetornado = { };
-//   regioes.forEach((regiao) => {
-//     const regis = species.filter((specie) => specie.location === regiao)
-//       .reduce((acc, curr) => {
-//         acc.push(({ [curr.name]: curr.residents.map((nomeAn) => nomeAn.name).sort() }));
-//         return acc;
-//       }, []);
-//     objetoRetornado[regiao] = regis;
-//   });
-//   return objetoRetornado;
-// };
+const verificaEAplica = (param) => {
+  if (!param || !param.includeNames) {
+    return valorBase(); // aqui eu chamo sem parametro
+  }
+  if (param.includeNames && param.sorted) {
+    const regioes = Object.keys(valorBase());
+    const objetoRetornado = { };
+    regioes.forEach((regiao) => {
+      const regis = species.filter((specie) => specie.location === regiao)
+        .reduce((acc, curr) => {
+          acc.push(({ [curr.name]: curr.residents.map((nomeAn) => nomeAn.name).sort() }));
+          return acc;
+        }, []);
+      objetoRetornado[regiao] = regis;
+    });
+    return objetoRetornado; // se o param tem includeNames e sorted
+  }
+  return includeNamesTrue(); // aqui retorna Se o parametro possui incluedName.
+};
 
-// const includeSexSorted = (param) => { // Se o parametro possui sex
-//   const regioes = Object.keys(notIncludedName());
-//   const objetoRetornado = { };
-//   regioes.forEach((regiao) => {
-//     const regis = species.filter((specie) => specie.location === regiao)
-//       .reduce((acc, curr) => {
-//         acc.push(({ [curr.name]: curr.residents
-//           .filter((resident) => resident.sex === param.sex)
-//           .map((teste) => teste.name).sort(),
-//         }));
-//         return acc;
-//       }, []);
-//     objetoRetornado[regiao] = regis;
-//   });
-//   return objetoRetornado;
-// };
+const includeSexSorted = (param) => {
+  const regioes = Object.keys(valorBase());
+  const objetoRetornado = { };
+  regioes.forEach((regiao) => {
+    const regis = species.filter((specie) => specie.location === regiao)
+      .reduce((acc, curr) => {
+        acc.push(({ [curr.name]: curr.residents
+          .filter((resident) => resident.sex === param.sex)
+          .map((teste) => teste.name).sort(),
+        }));
+        return acc;
+      }, []);
+    objetoRetornado[regiao] = regis;
+  });
+  return objetoRetornado;
+};
 
-// const includeSex = (param) => { // Se o parametro possui sex
-//   const regioes = Object.keys(notIncludedName());
-//   const objetoRetornado = { };
-//   if (param.sorted) {
-//     return includeSexSorted();
-//   }
-//   regioes.forEach((regiao) => {
-//     const regis = species.filter((specie) => specie.location === regiao)
-//       .reduce((acc, curr) => {
-//         acc.push(({ [curr.name]: curr.residents
-//           .filter((resident) => resident.sex === param.sex)
-//           .map((teste) => teste.name),
-//         }));
-//         return acc;
-//       }, []);
-//     objetoRetornado[regiao] = regis;
-//   });
-//   return objetoRetornado.NW;
-// };
+const includeSex = (param) => { // Se o parametro possui sex
+  const regioes = Object.keys(valorBase());
+  const objetoRetornado = { };
+  if (param.sorted) {
+    return includeSexSorted(param);
+  }
+  regioes.forEach((regiao) => {
+    const regis = species.filter((specie) => specie.location === regiao)
+      .reduce((acc, curr) => {
+        acc.push(({ [curr.name]: curr.residents
+          .filter((resident) => resident.sex === param.sex)
+          .map((teste) => teste.name),
+        }));
+        return acc;
+      }, []);
+    objetoRetornado[regiao] = regis;
+  });
+  return objetoRetornado;// ta sim, se liga eu testei agora, sei lá to tiltado
+};
 
-// console.log(includeSex({ includeNames: true, sex: 'female' }));
+console.log(includeSex({ sex: 'female', sorted: true }));
 
 function getAnimalMap(options) {
   if (!options) {
-    return notIncludedName();
+    return valorBase();
   }
-  // includeNames(options);
+  if (options.includeNames && options.sex) {
+    if (options.sorted) {
+      return includeSex(options);
+    }
+    return includeSex(options);
+  }
+  return verificaEAplica(options);
 }
+
 module.exports = getAnimalMap;
